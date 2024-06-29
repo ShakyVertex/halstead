0e1ed7da8c41fc047372c881f5124f165f086841
SellaMomota <sellamomota@outlook.com>
2024-06-29 03:23:22 -0400

    FileUploadController

--- lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
@@ -1,9 +1,74 @@
 package com.atguigu.lease.web.admin.service.impl;
 
+import com.atguigu.lease.common.minio.MinioProperties;
 import com.atguigu.lease.web.admin.service.FileService;
+import io.minio.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
 
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private MinioClient minioClient;
+
+    @Autowired
+    private MinioProperties minioProperties;
+
+    @Override
+    public String upload(MultipartFile file) {
+        try {
+            boolean bucketExists = minioClient.bucketExists(
+                    BucketExistsArgs.builder()
+                            .bucket(minioProperties.getBucketName())
+                            .build()
+            );
+            if (!bucketExists) {
+                minioClient.makeBucket(MakeBucketArgs.builder()
+                        .bucket(minioProperties.getBucketName())
+                        .build()
+                );
+                minioClient.setBucketPolicy(
+                        SetBucketPolicyArgs.builder()
+                                .bucket(minioProperties.getBucketName())
+                                .config(createBucketPolicyConfig(minioProperties.getBucketName()))
+                                .build()
+                );
+            }
+            String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+            minioClient.putObject(
+                    PutObjectArgs.builder()
+                            .bucket(minioProperties.getBucketName())
+                            .stream(file.getInputStream(), file.getSize(), -1)
+                            .object(filename)
+                            .contentType(file.getContentType())
+                            .build()
+            );
+
+            return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), filename);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String createBucketPolicyConfig(String bucketName) {
+
+        return """
+            {
+              "Statement" : [ {
+                "Action" : "s3:GetObject",
+                "Effect" : "Allow",
+                "Principal" : "*",
+                "Resource" : "arn:aws:s3:::%s/*"
+              } ],
+              "Version" : "2012-10-17"
+            }
+            """.formatted(bucketName);
+    }
 }
