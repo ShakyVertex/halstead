63017558844a9f68207c55763a584acdbe259e18
SellaMomota <sellamomota@outlook.com>
2024-07-01 16:31:27 -0400

    ViewAppointmentController

--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/controller/appointment/ViewAppointmentController.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/controller/appointment/ViewAppointmentController.java
@@ -1,12 +1,15 @@
 package com.atguigu.lease.web.app.controller.appointment;
 
 
+import com.atguigu.lease.common.login.LoginUserHolder;
 import com.atguigu.lease.common.result.Result;
 import com.atguigu.lease.model.entity.ViewAppointment;
+import com.atguigu.lease.web.app.service.ViewAppointmentService;
 import com.atguigu.lease.web.app.vo.appointment.AppointmentDetailVo;
 import com.atguigu.lease.web.app.vo.appointment.AppointmentItemVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,21 +19,28 @@ import java.util.List;
 @RequestMapping("/app/appointment")
 public class ViewAppointmentController {
 
+    @Autowired
+    private ViewAppointmentService service;
+
     @Operation(summary = "保存或更新看房预约")
     @PostMapping("/saveOrUpdate")
     public Result saveOrUpdate(@RequestBody ViewAppointment viewAppointment) {
+        viewAppointment.setUserId(LoginUserHolder.getLoginUser().getUserId());
+        service.saveOrUpdate(viewAppointment);
         return Result.ok();
     }
 
     @Operation(summary = "查询个人预约看房列表")
     @GetMapping("listItem")
     public Result<List<AppointmentItemVo>> listItem() {
-        return Result.ok();
+        List<AppointmentItemVo> list = service.listItemByUserId(LoginUserHolder.getLoginUser().getUserId());
+        return Result.ok(list);
     }
 
     @GetMapping("getDetailById")
     @Operation(summary = "根据ID查询预约详情信息")
     public Result<AppointmentDetailVo> getDetailById(Long id) {
+        AppointmentDetailVo appointmentDetailVo = service.getDetailById(id);
         return Result.ok();
     }
 
--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/ViewAppointmentServiceImpl.java
@@ -2,9 +2,17 @@ package com.atguigu.lease.web.app.service.impl;
 
 import com.atguigu.lease.model.entity.ViewAppointment;
 import com.atguigu.lease.web.app.mapper.ViewAppointmentMapper;
+import com.atguigu.lease.web.app.service.ApartmentInfoService;
 import com.atguigu.lease.web.app.service.ViewAppointmentService;
+import com.atguigu.lease.web.app.vo.apartment.ApartmentItemVo;
+import com.atguigu.lease.web.app.vo.appointment.AppointmentDetailVo;
+import com.atguigu.lease.web.app.vo.appointment.AppointmentItemVo;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author liubo
@@ -15,6 +23,31 @@ import org.springframework.stereotype.Service;
 public class ViewAppointmentServiceImpl extends ServiceImpl<ViewAppointmentMapper, ViewAppointment>
         implements ViewAppointmentService {
 
+    @Autowired
+    private ViewAppointmentMapper viewAppointmentMapper;
+
+    @Autowired
+    private ApartmentInfoService apartmentInfoService;
+
+    @Override
+    public List<AppointmentItemVo> listItemByUserId(Long userId) {
+        return viewAppointmentMapper.listItemByUserId(userId);
+    }
+
+    @Override
+    public AppointmentDetailVo getDetailById(Long id) {
+
+        ViewAppointment viewAppointment = viewAppointmentMapper.selectById(id);
+
+        ApartmentItemVo apartmentItemVo = apartmentInfoService.selectApartmentItemVoById(viewAppointment.getApartmentId());
+
+        AppointmentDetailVo agreementDetailVo = new AppointmentDetailVo();
+        BeanUtils.copyProperties(viewAppointment, agreementDetailVo);
+
+        agreementDetailVo.setApartmentItemVo(apartmentItemVo);
+
+        return agreementDetailVo;
+    }
 }
 
 
