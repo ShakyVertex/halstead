0218a49452d008b83fa2b8dc83687eaf54711eb4
SellaMomota <sellamomota@outlook.com>
2024-07-01 02:16:04 -0400

    SMS login controller

---
+++ lease/common/src/main/java/com/atguigu/lease/common/utils/CodeUtil.java
@@ -0,0 +1,17 @@
+package com.atguigu.lease.common.utils;
+
+import java.util.Random;
+
+public class CodeUtil {
+
+    public static String getRandomCode(Integer length) {
+
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            int num = random.nextInt(10);
+            builder.append(num);
+        }
+        return builder.toString();
+    }
+}
--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LoginServiceImpl.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/impl/LoginServiceImpl.java
@@ -1,9 +1,42 @@
 package com.atguigu.lease.web.app.service.impl;
 
+import com.atguigu.lease.common.constant.RedisConstant;
+import com.atguigu.lease.common.exception.LeaseException;
+import com.atguigu.lease.common.result.ResultCodeEnum;
+import com.atguigu.lease.common.utils.CodeUtil;
 import com.atguigu.lease.web.app.service.LoginService;
+import com.atguigu.lease.web.app.service.SmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginServiceImpl implements LoginService {
 
+    @Autowired
+    private SmsService smsService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Override
+    public void getCode(String phone) {
+
+        // 获取生成一个随机的code并生成redis key用于存储
+        String code = CodeUtil.getRandomCode(6);
+        String key = RedisConstant.APP_LOGIN_PREFIX + phone;
+
+        // 判断用户是否已经请求过验证码的生成
+        Boolean hasKey = redisTemplate.hasKey(key);
+        if (Boolean.TRUE.equals(hasKey)) {
+            Long ttl = redisTemplate.getExpire(key, TimeUnit.SECONDS);
+            if (RedisConstant.APP_LOGIN_CODE_TTL_SEC - ttl < RedisConstant.APP_LOGIN_CODE_RESEND_TIME_SEC) {
+                throw new LeaseException(ResultCodeEnum.APP_SEND_SMS_TOO_OFTEN);
+            }
+        }
+        smsService.sendCode(phone, code);
+        redisTemplate.opsForValue().set(key, code, RedisConstant.APP_LOGIN_CODE_TTL_SEC, TimeUnit.SECONDS);
+    }
 }
