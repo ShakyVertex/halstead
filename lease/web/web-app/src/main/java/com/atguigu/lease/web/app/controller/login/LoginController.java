96284832c9c3b8d1782a516d289a6caea6210e30
SellaMomota <sellamomota@outlook.com>
2024-07-01 03:40:01 -0400

    LoginController

--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/controller/login/LoginController.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/controller/login/LoginController.java
@@ -28,7 +28,8 @@ public class LoginController {
     @PostMapping("login")
     @Operation(summary = "登录")
     public Result<String> login(@RequestBody LoginVo loginVo) {
-        return Result.ok();
+        String token = service.login(loginVo);
+        return Result.ok(token);
     }
 
     @GetMapping("info")
--- lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/LoginService.java
+++ lease/web/web-app/src/main/java/com/atguigu/lease/web/app/service/LoginService.java
@@ -1,5 +1,9 @@
 package com.atguigu.lease.web.app.service;
 
+import com.atguigu.lease.web.app.vo.user.LoginVo;
+
 public interface LoginService {
     void getCode(String phone);
+
+    String login(LoginVo loginVo);
 }
